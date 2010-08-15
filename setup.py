cf663a5df3fa1b1fdc43ec6fb857a4d280ebc7d1
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2010-08-15 21:12:25 +1000

    Initial package structure.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils import core
+
+packages = [
+    "wsgi2wsgi",
+    "wsgi2wsgi.adapters",
+    "wsgi2wsgi.adapters.cgi2wsgi",
+]
+
+core.setup(
+    name = "cgi2wsgi",
+    version = "1.0",
+    packages = packages,
+    license = "Apache 2.0 Licence",
+    description = "WSGI deployment toolkit.",
+    author = "Graham Dumpleton",
+    author_email = "Graham.Dumpleton@gmail.com",
+    maintainer = "Graham Dumpleton",
+    maintainer_email = "Graham.Dumpleton@gmail.com",
+    url = "http://blog.dscpl.com.au",
+)
